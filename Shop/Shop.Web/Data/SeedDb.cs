ccd62f8ea89c53a1b2be92d72e4f49aa2e60eeee
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2019-07-26 12:22:09 -0300

    Add Users. Users got added, modify the db, and some others classes.

--- Shop/Shop.Web/Data/SeedDb.cs
+++ Shop/Shop.Web/Data/SeedDb.cs
@@ -1,46 +1,72 @@
 ﻿
 namespace Shop.Web.Data
 {
+    using Entities;
+    using Microsoft.AspNetCore.Identity;
     using System;
     using System.Linq;
     using System.Threading.Tasks;
-    
-    using Entities;
 
     public class SeedDb
     {
         private readonly DataContext context;
+        private readonly UserManager<User> userManager;
         private Random random;
 
-        public SeedDb(DataContext context)
+        public string PhoneNumber { get; private set; }
+
+        public SeedDb(DataContext context, UserManager<User> userManager)
         {
             this.context = context;
+            this.userManager = userManager;
             this.random = new Random();
         }
 
         public async Task SeedAsync()
         {
+
             await this.context.Database.EnsureCreatedAsync();
 
+            var user = await this.userManager.FindByEmailAsync("diegohenderson425@outlook.com");
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = "Diego",
+                    LastName = "Henderson  ",
+                    Email = "diegohenderson425@outlook.com",
+                    UserName = "diegohenderson425@outlook.com",
+                    PhoneNumber = "3512688446",
+                };
+                var result = await this.userManager.CreateAsync(user, "123456");
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create the user in seeder");
+                }
+            };
             if (!this.context.Products.Any())
             {
-                this.AddProduct("First Product");
-                this.AddProduct("Second Product");
-                this.AddProduct("Third Product");
+                this.AddProduct("First Product",user);
+                this.AddProduct("Second Product",user);
+                this.AddProduct("Third Product",user);
                 await this.context.SaveChangesAsync();
             }
         }
 
-        private void AddProduct(string name)
+        private void AddProduct(string name, User user)
         {
             this.context.Products.Add(new Product
             {
                 Name = name,
                 Price = this.random.Next(100),
                 IsAvailabe = true,
-                Stock = this.random.Next(100)
+                Stock = this.random.Next(100),
+                User = user,
             });
         }
+
     }
 
 }
+
+
